2d6ebe54d4672d885b5da51d6412e3292e05eacb
installer <installer@gmail.com>
2025-06-30 21:42:15 -0500

    First Commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "KooL's NixOS-Hyprland"; 
+  	
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  	#nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	
+	#hyprland.url = "github:hyprwm/Hyprland"; # hyprland development
+	#distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
+	ags.url = "github:aylur/ags/v1"; # aylurs-gtk-shell-v1
+  	};
+
+  outputs = 
+	inputs@{ self, nixpkgs, ags, ... }:
+    	let
+      system = "x86_64-linux";
+      host = "dylan-lenovo";
+      username = "dylan";
+
+    pkgs = import nixpkgs {
+       	inherit system;
+       	config = {
+       	allowUnfree = true;
+       	};
+      };
+    in
+      {
+	nixosConfigurations = {
+      "${host}" = nixpkgs.lib.nixosSystem rec {
+		specialArgs = { 
+			inherit system;
+			inherit inputs;
+			inherit username;
+			inherit host;
+			};
+	   		modules = [ 
+				./hosts/${host}/config.nix 
+				# inputs.distro-grub-themes.nixosModules.${system}.default
+				];
+			};
+		};
+	};
+}
